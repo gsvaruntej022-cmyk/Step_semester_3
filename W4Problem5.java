7c07ec0e56acbf2265740eeee5d6efabb358e9b0
varun <gsvaruntej022@gmail.com>
2026-09-05 21:14:14 +0530

    Add Problem5 find minimum term

---
+++ W4Problem5.java
@@ -0,0 +1,37 @@
+public class W4Problem5 {
+
+    static int findMin(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+
+            // Already sorted
+            if (nums[left] < nums[right]) {
+                return nums[left];
+            }
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                // Minimum is in the right half
+                left = mid + 1;
+            } else {
+                // Minimum is in the left half
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {3, 4, 5, 1, 2};
+
+        int result = findMin(nums);
+
+        System.out.println("Minimum element: " + result);
+    }
+}
